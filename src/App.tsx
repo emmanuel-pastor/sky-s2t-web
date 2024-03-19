2cab890a422b0a638a83376ce85e0feed7a0e688
Emmanuel Pastor <ommanu13@hotmail.fr>
2024-03-19 23:09:57 +0100

    Add GoogleLogin button

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import {useEffect, useState} from "react";
 import {Button} from "@/components/ui/button.tsx";
 import {SpeechIcon, StopCircleIcon} from "lucide-react";
 import SpeechRecognition from "@/lib/SpeechRecognition.ts";
+import {CredentialResponse, GoogleLogin} from "@react-oauth/google";
 
 function App() {
   const [displayText, setDisplayText] = useState('');
   const [isListening, setIsListening] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
   const recognizer: SpeechRecognition = SpeechRecognition.getInstance(import.meta.env.VITE_SPEECH_KEY, import.meta.env.VITE_SPEECH_REGION);
 
+  // Stops the recognition when the tab is hidden
   useEffect(() => {
     const handleVisibilityChange = () => {
       if (document.visibilityState === 'hidden') {
@@ -65,21 +68,38 @@ function App() {
     recognizer.stopRecognition();
   }
 
+  function onGoogleLoginSuccess(credentialResponse: CredentialResponse) {
+    console.log('Login Success:', credentialResponse);
+    setSignedIn(true);
+  }
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <div className="row max-w-6xl">
-        {isListening ?
-          <Button onClick={onStopClicked}>
-            <StopCircleIcon className="mr-2 h-4 w-4"/> Stop
-          </Button>
-          :
-          <Button onClick={onStartClicked}>
-            <SpeechIcon className="mr-2 h-4 w-4"/> Start
-          </Button>
-        }
-        <div className="mt-8 text-3xl text-left leading-snug">
-          {displayText}
-        </div>
+      <div className="row  max-w-6xl">
+        {signedIn ? (<>
+          {isListening ?
+            <Button onClick={onStopClicked}>
+              <StopCircleIcon className="mr-2 h-4 w-4"/> Stop
+            </Button>
+            :
+            <Button onClick={onStartClicked}>
+              <SpeechIcon className="mr-2 h-4 w-4"/> Start
+            </Button>
+          }
+          <div className="mt-8 text-3xl text-left leading-snug">
+            {displayText}
+          </div>
+        </>) : (
+          <GoogleLogin
+            shape={"pill"}
+            theme="filled_blue"
+            use_fedcm_for_prompt={true}
+            onSuccess={onGoogleLoginSuccess}
+            onError={() => {
+              console.log('Login Failed:');
+            }}
+          />
+        )}
       </div>
     </ThemeProvider>
   )
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_SPEECH_KEY: string,
   readonly  VITE_SPEECH_REGION: string,
+  readonly VITE_GOOGLE_CLIENT_ID: string
 }
 
 interface ImportMeta {
